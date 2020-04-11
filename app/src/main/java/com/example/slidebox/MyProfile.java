1aebc7d01c1cebef38d6eb19782a696b35c563f2
Niulei <niulei54@gmail.com>
2020-04-11 15:38:44 +0100

    assf

--- app/src/main/java/com/example/slidebox/MyProfile.java
+++ app/src/main/java/com/example/slidebox/MyProfile.java
@@ -1,8 +1,15 @@
 package com.example.slidebox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+
+import static androidx.navigation.ui.NavigationUI.*;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -11,4 +18,8 @@ public class MyProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
     }
+
+
+
+
 }
