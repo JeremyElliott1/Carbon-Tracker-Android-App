b80bab872f9219c35cd504483d7e5e4e2607d2ae
ScottHamo <S.Hamilton5@newcastle.ac.uk>
2020-04-16 23:18:38 +0100

    Linhan Changes - Help and Travel

--- app/src/main/java/com/example/slidebox/HelpOptions.java
+++ app/src/main/java/com/example/slidebox/HelpOptions.java
@@ -6,16 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HelpOptions extends AppCompatActivity {
     private Button button_back;
-    private Button help;
+    private Button about;
+    private Button shop;
+    private Button leaderboard;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.options_help);
+        textView = (TextView)findViewById(R.id.textView10);
+
         button_back = (Button)findViewById(R.id.button4);
         button_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,11 +31,27 @@ public class HelpOptions extends AppCompatActivity {
             }
         });
 
-        help = (Button)findViewById(R.id.button);
-        help.setOnClickListener(new View.OnClickListener() {
+        about = (Button)findViewById(R.id.button);
+        about.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Should add content", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                textView.setText("This app is for reuseable, recycleable and environmental protection");
+            }
+        });
+
+        shop = (Button)findViewById(R.id.button6);
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Users can use point to exchange certain items");
+            }
+        });
+
+        leaderboard = (Button)findViewById(R.id.button7);
+        leaderboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Leaderboard shows users' points for you to compare");
             }
         });
     }
