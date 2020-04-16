0adec509198a79589de5cb5eb75e29b67fb3f78c
Gideon Bayisa <Gbayisa@hotmail.com>
2020-04-16 14:25:05 +0100

    Gideon 4/16/20

--- app/src/main/java/com/example/slidebox/ui/recyclables/RecyclablesViewModel.java
+++ app/src/main/java/com/example/slidebox/ui/recyclables/RecyclablesViewModel.java
@@ -4,11 +4,11 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class recyclablesViewModel extends ViewModel {
+public class RecyclablesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public recyclablesViewModel() {
+    public RecyclablesViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is Recyclables fragment");
     }
