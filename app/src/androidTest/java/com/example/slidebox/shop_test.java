9ee0c74b571c1d09d2507e5001b79e36e5984463
lickyzero <63852879+lickyzero@users.noreply.github.com>
2020-04-29 04:41:42 +0100

    need to add id to each toast to achieve path coverage

---
+++ app/src/androidTest/java/com/example/slidebox/shop_test.java
@@ -0,0 +1,26 @@
+package com.example.slidebox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static junit.framework.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class shop_test
+{
+    /*@Test
+    public void testBuilder_Activity_Resource()
+    {
+        ActivityScenario<EmptyTestActivity> scenario = ActivityScenario.launch(EmptyTestActivity.class);
+        scenario.onActivity(activity -> {
+            final MaterialTapTargetPrompt.Builder builder = new MaterialTapTargetPrompt.Builder(activity, 0);
+            assertTrue(builder.getResourceFinder() instanceof ActivityResourceFinder);
+        });
+    }*/
+
+
+}
