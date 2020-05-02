9118952b871ee564fef49c1f793fc5a717e18a1c
Niulei <niulei54@gmail.com>
2020-05-02 19:51:20 +0100

    read image from CloudStore add thumbup onClickListener write the number of thumb to database
    need -- synchronization numthumb

--- app/src/main/java/com/example/slidebox/ui/leaderboard/RankingAdapter.java
+++ app/src/main/java/com/example/slidebox/ui/leaderboard/RankingAdapter.java
@@ -12,13 +12,24 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter;
 import com.firebase.ui.firestore.paging.FirestorePagingOptions;
 import com.firebase.ui.firestore.paging.LoadingState;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPointsViewHolder> {
 
@@ -28,6 +39,11 @@ public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPoint
     private static final int WEEK_FLAG = 2;
     private static final int MONTH_FLAG = 3;
     private int flag;
+    private int count = 0;
+    private OnThumbupClick onThumbupClick;
+    private DocumentSnapshot ds;
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     /**
      * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
@@ -35,9 +51,11 @@ public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPoint
      *
      * @param options
      */
-    public RankingAdapter(@NonNull FirestorePagingOptions<UserPoints> options, int flag) {
+    public RankingAdapter(@NonNull FirestorePagingOptions<UserPoints> options, int flag, OnThumbupClick onThumbupClick) {
         super(options);
         this.flag = flag;
+        this.onThumbupClick = onThumbupClick;
+        readDocSnapshot();
     }
 
     @NonNull
@@ -45,18 +63,7 @@ public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPoint
     public UserPointsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_rangkinglist,parent,false);
 
-        ImageView imageViewThumbup = view.findViewById(R.id.imageViewThumbup);
-        imageViewThumbup.setOnClickListener(new View.OnClickListener() {
-                                                @Override
-                                                public void onClick(View v) {
-
-
-                                                }
-                                            }
-        );
-
-
-        return new UserPointsViewHolder(view);
+        return new UserPointsViewHolder(view,onThumbupClick);
     }
 
     @Override
@@ -74,10 +81,72 @@ public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPoint
                 point = String.valueOf(model.getMonthlyPoints());
                 break;
         }
+        //holder.onClick(model.getNumThumbup());
+
         holder.getFullName().setText(fullName);
         holder.getImageViewThumbup().setImageResource(R.drawable.ic_thumb_up_black_24dp);
         holder.getItemPosition().setText(String.valueOf(position +1));
         holder.getPoints().setText(point);
+
+        DocumentSnapshot documentSnapshot = getItem(position);
+
+        if(documentSnapshot.get(user.getUid()) != null){
+            Log.d(TAG, "get thumb doc" + documentSnapshot.get(user.getUid()));
+            String o = (String) documentSnapshot.get(user.getUid());
+
+            // holder.getNumThumbup().setText(String .valueOf(holder.onClick(o,documentSnapshot)));
+        }else {
+            if( String.valueOf(model.getNumThumbup()).isEmpty() == false){
+                holder.getNumThumbup().setText(String .valueOf(model.getNumThumbup()));
+            }
+        }
+
+
+    }
+
+    private void readDocSnapshot(){
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore.collection("users").document("thumbup").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        ds = document;
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        //        firebaseFirestore.collection("users").document("thumbup").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//                            ds =document;
+////                            int numthumbup = (int) ds.get(fullname2);
+//                            Log.d(TAG, "get thumb doc" + numthumbup );
+//                            holder1.getNumThumbup().setText(String .valueOf(holder1.onClick(numthumbup,documentSnapshot)));
+//
+//                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+//                    } else {
+//                        Log.d(TAG, "No such document");
+//                    }
+//                } else {
+//                    Log.d(TAG, "get failed with ", task.getException());
+//                }
+//            }
+//        });
+
+
+
     }
 
     @Override
@@ -88,5 +157,13 @@ public class RankingAdapter extends FirestorePagingAdapter<UserPoints, UserPoint
                 Log.d(TAG,"All item loaded " + getItemCount());
                 break;
         }
+    }
+
+    public OnThumbupClick getOnThumbupClick() {
+        return onThumbupClick;
+    }
+
+    public interface OnThumbupClick{
+        void onClick();
     }
 }
--- app/src/main/java/com/example/slidebox/ui/leaderboard/UserPointsViewHolder.java
+++ app/src/main/java/com/example/slidebox/ui/leaderboard/UserPointsViewHolder.java
@@ -1,5 +1,6 @@
 package com.example.slidebox.ui.leaderboard;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,22 +9,31 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.slidebox.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
-class UserPointsViewHolder extends RecyclerView.ViewHolder {
+import java.util.HashMap;
+import java.util.Map;
 
+class UserPointsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    private static String TAG = "Holder";
     private TextView fullName;
     private TextView points;
     private ImageView imageViewThumbup;
     private TextView numThumbup;
     private TextView itemPosition;
     private ImageView imageViewUser;
+    private RankingAdapter.OnThumbupClick onThumbupClick;
 
 
 
-    public UserPointsViewHolder(@NonNull View itemView) {
+    public UserPointsViewHolder(@NonNull View itemView, RankingAdapter.OnThumbupClick onThumbupClick) {
         super(itemView);
         fullName = itemView.findViewById(R.id.textViewUserName);
         points = itemView.findViewById(R.id.textViewPoints);
@@ -31,7 +41,7 @@ class UserPointsViewHolder extends RecyclerView.ViewHolder {
         numThumbup = itemView.findViewById(R.id.textViewThumbupNum);
         itemPosition = itemView.findViewById(R.id.textViewPosition);
         imageViewUser = itemView.findViewById(R.id.imageViewUser);
-
+        this.onThumbupClick = onThumbupClick;
     }
 
     public TextView getItemPosition() {
@@ -56,5 +66,39 @@ class UserPointsViewHolder extends RecyclerView.ViewHolder {
 
     public TextView getNumThumbup() {
         return numThumbup;
+    }
+
+    public int onClick(final String o, final DocumentSnapshot documentSnapshot) {
+        final int[] i1 = {Integer.valueOf(o)};
+        imageViewThumbup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i1[0]++ ;
+                FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String fullname = user.getUid();
+                Log.d(TAG, "Count is " + i1[0]);
+                Map<String, Object> map = new HashMap<>();
+                map.put(fullname,String.valueOf(i1[0]));
+                firebaseFirestore.collection("users").document(user.getUid()).update(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Added thumbup! " );
+                    }
+                });
+                firebaseFirestore.collection("users").document("thumbup").update(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Added thumbup! " );
+                    }
+                });
+            }
+        });
+        return i1[0];
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
